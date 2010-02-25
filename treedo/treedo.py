0dd7ec4554e239f16b081e1e8f7db5581215ef19
Josh VanderLinden <jvanderlinden@sciencelogic.com>
2010-02-25 17:38:31 -0500

    Updated the main() function to "cd" to the project's directory, so we can use images.

--- treedo/treedo.py
+++ treedo/treedo.py
@@ -2,14 +2,18 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Josh VanderLinden'
-__appname__ = 'TreeDo'
 __version__ = '0.2'
 
 def main():
     from gui import TreeDoFrame
     import gettext
+    import os
     import wx
     gettext.install('treedo')
+
+    # change to the project's directory, so we can use our images
+    dirname = os.path.abspath(os.path.dirname(__file__))
+    os.chdir(dirname)
 
     treedo = wx.PySimpleApp()
     frame = TreeDoFrame()
